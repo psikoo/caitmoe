db76b17dc7e6e9b62c8329b013c13882537cd67c
psikoo <91342186+psikoo@users.noreply.github.com>
2025-08-02 06:13:06 +0200

    done

--- nest-caitmoe/src/modules/www/www.service.ts
+++ nest-caitmoe/src/modules/www/www.service.ts
@@ -2,49 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 
-let tamagotchi = {
-  name: "Tamago :3",
-  state:"Alive",
-  bornTime: new Date().valueOf(),
-  deadTime: new Date().valueOf(),
-  happiness: 10,
-  hunger: 10,
-  energy: 10,
-  lastUpdate: new Date().valueOf()
-}
-
 @Injectable()
 export class wwwService {
   constructor(private readonly configService: ConfigService) {}
   async getWeather() {
+    let finalResponse = "empty";
     let config = {
       method: "get",
       url: "https://api.openweathermap.org/data/2.5/weather?location=Madrid&unit=metric&appid="+this.configService.get("WEATHER_KEY"),
       headers: { "Content-Type": "application/json" }
     };
-    await axios.request(config) .then((response) => {
-      return JSON.parse(JSON.stringify(response.data));
-    }) .catch((error) => {
-      return JSON.parse(JSON.stringify(error));
-    });
-    return JSON.parse(JSON.stringify("error"));
+    await axios.request(config) 
+    .then((response) => { finalResponse = response.data; })
+    .catch((error) => { finalResponse = error; });
+    return JSON.parse(JSON.stringify(finalResponse));
   }
 
   async getLastFM(): Promise<JSON> {
+    let finalResponse = "empty";
     let config = {
       method: "get",
       url: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=psikooUwU&limit=1&format=json&api_key="+this.configService.get("LASTFM_KEY"),
       headers: { "Content-Type": "application/json" }
     };
-    let finalResponse = "empty";
-    await axios.request(config) .then((response) => {
-      console.log(response.data);
-      finalResponse = response.data;
-    }).catch((error) => {
-      console.log(error);
-      finalResponse = error;
-    });
-    console.log("after req")
+    await axios.request(config) 
+    .then((response) => { finalResponse = response.data; })
+    .catch((error) => { finalResponse = error; });
     return JSON.parse(JSON.stringify(finalResponse));
   }
 }
